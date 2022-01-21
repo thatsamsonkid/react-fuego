b9dd1ccc2fb818665fe6b0ae1c4b7d7c1c21f172
Sammy Mohamed <sammymohamed1223@gmail.com>
2022-01-21 01:37:04 -0500

    FormField Import Fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export * from "./components/button/Button";
 export * from "./components/card/Card";
 
-export * from "./components/formField";
+export * from "./components/formField/Field";
+// export * from "./components/formField/Field-Fix";
 
 export * from "./components/modal/Modal";
 export * from "./components/modal/useModal";
