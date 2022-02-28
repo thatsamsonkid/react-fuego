85e192426115bd3238b23d67355ed12ffc235a23
Sammy Mohamed <sammymohamed1223@gmail.com>
2022-02-28 09:43:06 -0500

    Minor Update to Tabs Story

--- src/components/tabs/Tabs.stories.tsx
+++ src/components/tabs/Tabs.stories.tsx
@@ -1,33 +1,32 @@
-// import React from "react";
-// import { ComponentStory, ComponentMeta } from "@storybook/react";
-// import { Tabs } from "./Tabs";
-// import { Tab } from "./Tab";
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Tabs } from "./Tabs";
+import { Tab } from "./Tab";
 
-// export default {
-//     title: "Components/Tabs",
-//     component: Tabs,
-//     argTypes: {
-//         // backgroundColor: { control: "color" },
-//     },
-// } as ComponentMeta<typeof Tabs>;
+export default {
+  title: "Components/Tabs",
+  component: Tabs,
+  argTypes: {
+    // backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof Tabs>;
 
-// const Template: ComponentStory<typeof Tabs> = (args) => <Tabs {...args} />;
-// const chillens = (<>
-//     <Tab label="tab1">Tab 1</Tab>
-//     <Tab label="tab2">Tab 2</Tab>
-// </>
-// );
+const Template: ComponentStory<typeof Tabs> = (args) => (
+  <Tabs {...args}>
+    <Tab label="tab1">Tab 1</Tab>
+    <Tab label="tab2">Tab 2</Tab>
+  </Tabs>
+);
 
-// export const Default = Template.bind({});
-// Default.args = {
+export const Default = Template.bind({});
+Default.args = {};
+
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   options: options,
 // };
 
-// // export const Secondary = Template.bind({});
-// // Secondary.args = {
-// //   options: options,
-// // };
-
-// // export const Tertiary = Template.bind({});
-// // Tertiary.args = {
-// //   options: options,
-// // };
+// export const Tertiary = Template.bind({});
+// Tertiary.args = {
+//   options: options,
+// };
