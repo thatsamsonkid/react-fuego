8c6a1efcca34d048afc9676ad523113b0105e8ea
Sammy Mohamed <sammymohamed1223@gmail.com>
2022-02-28 21:31:42 -0500

    Tab Export

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from "./components/card/Card";
 export * from "./components/formFields/field/Field";
 export * from "./components/formFields/select/Select";
 export * from "./components/formFields/listbox/Listbox";
+export * from "./components/tabs";
 
 export * from "./components/modal/Modal";
 export * from "./components/modal/useModal";
