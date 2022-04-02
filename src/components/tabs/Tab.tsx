6d5b4aef2c8ff4e19425d82d6988df76ad93fc10
Sammy Mohamed <sammymohamed1223@gmail.com>
2022-04-02 00:07:23 -0400

    Change from Label to ID for Active Tab State

--- src/components/tabs/Tab.tsx
+++ src/components/tabs/Tab.tsx
@@ -37,7 +37,7 @@ const TabComponent = forwardRef(
     }: ITab,
     ref: any
   ) => {
-    const isActiveTab = () => (activeTab === label ? true : false);
+    const isActiveTab = () => (activeTab === tabId ? true : false);
     const tabClasses = classnames(
       {
         "sm-tab--active": isActiveTab(),
